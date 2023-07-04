aa08629cec493d235f311b19f49884d9b84f1f1d
mini-niko <119255200+mini-niko@users.noreply.github.com>
2023-07-04 20:20:42 -0300

    Update .sql

--- sorveteriafx.sql
+++ sorveteriafx.sql
@@ -3,7 +3,15 @@ USE senac_sorveteriafx;
 
 CREATE TABLE sorvete(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    sabor VARCHAR(100) NOT NULL,
+    sabor VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE venda_sorvete(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id_sabor BIGINT NOT NULL,
     quantidade DOUBLE NOT NULL,
-    data_entrada DATE NOT NULL
+    data_venda DATE NOT NULL,
+    preco DOUBLE NOT NULL,
+    
+    FOREIGN KEY (id_sabor) REFERENCES sorvete(id)
 )
